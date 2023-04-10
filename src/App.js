c24bf02504c15df15a1b070dc3492ee3c9569ab7
Mark <mark.nelubin@gmail.com>
2023-04-10 19:54:29 +0700

    Условная отрисовка

--- src/App.js
+++ src/App.js
@@ -1,31 +1,20 @@
-import Avatar from './Props/Avatar';
-function Card({ children }) {
-  return <div className="card">{children}</div>;
+function Item({ name, isPacked }) {
+  let itemContent = name;
+  if (isPacked) {
+    itemContent = <del>{name + '✔'}</del>;
+  }
+  return <li className="item">{itemContent}</li>;
 }
-export default function Profile() {
+
+export default function PackingList() {
   return (
-    <Card>
-      <Avatar
-        size={100}
-        person={{
-          name: 'Katsuko Saruhashi',
-          imageId: 'YfeOqp2',
-        }}
-      />
-      <Avatar
-        size={80}
-        person={{
-          name: 'Aklilu Lemma',
-          imageId: 'OKS67lh',
-        }}
-      />
-      <Avatar
-        size={50}
-        person={{
-          name: 'Lin Lanying',
-          imageId: '1bX5QH6',
-        }}
-      />
-    </Card>
+    <section>
+      <h1>Sally Ride's Packing List</h1>
+      <ul>
+        <Item isPacked={true} name="Space suit" />
+        <Item isPacked={true} name="Helmet with a golden leaf" />
+        <Item isPacked={false} name="Photo of Tam" />
+      </ul>
+    </section>
   );
 }
