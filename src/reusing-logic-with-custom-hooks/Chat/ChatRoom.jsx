d6a925666ebcb737718700778d73fbad29964466
Nelyubin Mark <mark.nelubin@gmail.com>
2024-07-15 21:14:03 +0300

    Chat room with custom hook

--- src/reusing-logic-with-custom-hooks/Chat/ChatRoom.jsx
+++ src/reusing-logic-with-custom-hooks/Chat/ChatRoom.jsx
@@ -1,22 +1,9 @@
 import { useState, useEffect } from 'react';
-import { showNotification } from './notifications.js';
-import { createConnection } from './chat.js';
+import { useChatRoom } from './useChatRoom';
 
 export function ChatRoom({ roomId }) {
   const [serverUrl, setServerUrl] = useState('https://localhost:1234');
-
-  useEffect(() => {
-    const options = {
-      serverUrl: serverUrl,
-      roomId: roomId,
-    };
-    const connection = createConnection(options);
-    connection.on('message', (msg) => {
-      showNotification('New message: ' + msg);
-    });
-    connection.connect();
-    return () => connection.disconnect();
-  }, [roomId, serverUrl]);
+  useChatRoom({ serverUrl, roomId });
 
   return (
     <>
---
+++ src/reusing-logic-with-custom-hooks/Chat/useChatRoom.js
@@ -0,0 +1,18 @@
+import { useEffect } from 'react';
+import { showNotification } from './notifications.js';
+import { createConnection } from './chat.js';
+
+export function useChatRoom({ serverUrl, roomId }) {
+  useEffect(() => {
+    const options = {
+      serverUrl: serverUrl,
+      roomId: roomId,
+    };
+    const connection = createConnection(options);
+    connection.on('message', (msg) => {
+      showNotification('New message: ' + msg);
+    });
+    connection.connect();
+    return () => connection.disconnect();
+  }, [roomId, serverUrl]);
+}
