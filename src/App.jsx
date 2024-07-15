477758e44858885f7b92e66faa3eb9aca89c224c
Nelyubin Mark <mark.nelubin@gmail.com>
2024-07-15 12:05:18 +0300

    form without hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import SaveButton from './reusing-logic-with-custom-hooks/SaveButton';
 import StatusBar from './reusing-logic-with-custom-hooks/StatusBar';
+import Form from './reusing-logic-with-custom-hooks/Form';
 
 export default function App() {
   return (
     <>
-      <StatusBar />
-      <SaveButton />
+      {/* <StatusBar /> */}
+      {/* <SaveButton /> */}
+      <Form />
     </>
   );
 }
